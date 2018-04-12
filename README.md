4842825b49eec67c914838dd297cebc3cc4f0c9a
angelo <angel@angelengineering.com>
2018-04-12 11:58:17 +0700

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This contains the source files for my blog post https://blog.angelengineering.com/animated-nativescript-login-splashscreen/ which describes how to create a new app, add a custom splashscreen and icon, and animate the transition to an example login page.
+This contains the source files for my blog post https://blog.angelengineering.com/hybrid-animation/ which describes how to create a new app, add a custom splashscreen and icon, and animate the transition to an example login page using both CSS and Javascript Nativescript animation methods.
 
 
 tns init
--- app/main-page.js
+++ app/main-page.js
@@ -1,13 +1,88 @@
+const animationModule = require("tns-core-modules/ui/animation");
+const enumsModule = require("ui/enums");
+
 var currentPage;
 
-function setEditable() {
+function setEditable(args) {
   var un = currentPage.getViewById("fieldEmail");
   var p = currentPage.getViewById("fieldPassword");
   un.editable = true;
   p.editable = true;
+  //clear the ontap handler
+  un.off("tap");
+  p.off("tap");
+  //set focus on field tapped
+  args.object.focus();
 }
 exports.setEditable = setEditable;
 
 exports.onLoaded = function(args) {
-    currentPage = args.object;
-}
+  currentPage = args.object;
+
+  var fieldEmail = currentPage.getViewById("fieldEmail");
+  var fieldPassword = currentPage.getViewById("fieldPassword");
+  var buttonLogin = currentPage.getViewById("buttonLogin");
+  var logoGopher = currentPage.getViewById("logoGopher");
+  
+  var totalAnimTime = 1500; //all animation times are in milliseconds
+
+
+  var animations = [
+    {
+      target: logoGopher,
+      opacity: 1,
+      translate: {
+        x: 0,
+        y: 0
+      },
+      duration: totalAnimTime,
+      delay: 500,
+      curve: enumsModule.AnimationCurve.easeInOut
+    },
+    {
+      target: fieldEmail,
+      opacity: 1,
+      translate: {
+        x: 0,
+        y: 0
+      },
+      duration: totalAnimTime,
+      delay: 550,
+      curve: enumsModule.AnimationCurve.easeInOut
+    },
+    {
+      target: fieldPassword,
+      opacity: 1,
+      translate: {
+        x: 0,
+        y: 0
+      },
+      duration: totalAnimTime,
+      delay: 550,
+      curve: enumsModule.AnimationCurve.easeInOut
+    },
+    {
+      target: buttonLogin,
+      opacity: 1,
+      translate: {
+        x: 0,
+        y: 0
+      },
+      duration: totalAnimTime,
+      delay: 600,
+      curve: enumsModule.AnimationCurve.easeInOut
+    }
+  ];
+  introAnim = new animationModule.Animation(animations);
+  introAnim
+    .play()
+    .then(() => {
+      console.log("Animations finished");      
+    })
+    .catch(e => {
+      console.log("Caught ERROR during animation:");
+      console.log(e.message);      
+    });
+
+    
+};
